159c1027004e5ca6227b618538845571e75e10fd
Paul Aji <paulajiparayil123@gmail.com>
2023-08-27 11:49:01 +0530

    registeruser to work

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,10 +25,9 @@ from datetime import timedelta
 SECRET_KEY = 'django-insecure-1jg)g$y6tx8fvc5#&4$o-js@c)sk0$9j+v%say7-0)%p_(-kwl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# for url logic to work
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
